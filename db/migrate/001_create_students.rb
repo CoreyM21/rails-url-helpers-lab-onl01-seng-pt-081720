5136a2d3bb7a231bf169dd2deb886580750fd7dc
Corey Martin <coreyjmartin2@gmail.com>
2021-01-14 02:59:18 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,10 +1,1 @@
-class CreateStudents < ActiveRecord::Migration
-  def change
-    create_table :students do |t|
-      t.string :first_name
-      t.string :last_name
-
-      t.timestamps null: false
-    end
-  end
-end
+001_create_students.rb
